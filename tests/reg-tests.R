eef0989218fb39857fbb1a84a227d6a117948713
Roger D. Peng <rpeng@jhsph.edu>
2007-04-04 15:36:05 -0400

    Merge reg-tests2 and reg-tests3 into a single file

--- tests/reg-tests.R
+++ tests/reg-tests.R
@@ -199,3 +199,32 @@ for(type in types) {
     stopifnot(identical(numbers, b))
 }
 
+
+
+## Other tests
+
+rm(list = ls())
+
+
+dbCreate("testLoadingDB", "DB1")
+db <- dbInit("testLoadingDB", "DB1")
+
+set.seed(234)
+
+db$a <- rnorm(100)
+db$b <- runif(1000)
+
+dbLoad(db)  ## 'a', 'b'
+summary(a)
+summary(b)
+
+rm(list = ls())
+db <- dbInit("testLoadingDB", "DB1")
+
+dbLazyLoad(db)
+
+summary(a)
+summary(b)
+
+
+
