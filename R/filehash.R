06f8deae1c9f428154884442b822902ff1aa6efc
Roger D. Peng [audrey] <rpeng@jhsph.edu>
2007-04-10 09:06:07 -0400

    Remove some cruft and outdated/commented out code
    Also, remove dbNext and dbFirst generics.  They were never exported
    and I don't think every implemented.

--- R/filehash.R
+++ R/filehash.R
@@ -71,8 +71,6 @@ filehashFormats <- function(...) {
 
 createEmptyFile <- function(name) {
     ## If the file already exists, it is overwritten
-    ## con <- file(name, "wb")
-    ## close(con)
     file.create(name)
 }
 
@@ -107,14 +105,6 @@ setMethod("dbCreate", "ANY",
               TRUE
           })
           
-## dbCreate <- function(dbName, type) {
-##     if(missing(type))
-##         type <- filehashOption()$defaultType
-##     
-##     dbStartup(dbName, type, "create")
-##     TRUE
-## }
-
 setGeneric("dbInit", function(db, ...) standardGeneric("dbInit"))
 
 setMethod("dbInit", "ANY",
@@ -123,16 +113,6 @@ setMethod("dbInit", "ANY",
                   type <- filehashOption()$defaultType
               dbStartup(db, type, "initialize")
           })
-
-dbInitialize <- function(dbName, type) {
-    .Deprecated("dbInit")
-}
-
-## dbInit <- dbInitialize <- function(dbName, type) {
-##     if(missing(type))
-##         type <- filehashOption()$defaultType
-##     dbStartup(dbName, type, "initialize")
-## }
 
 ######################################################################
 ## Set options and retrieve list of options
@@ -196,7 +176,6 @@ setMethod("dbLazyLoad", "filehash",
                   keys <- dbList(db)
               else if(!is.character(keys))
                   stop("'keys' should be a character vector")
-              ## expr <- quote(dbFetch(db, key))
               
               wrap <- function(x, env) {
                   key <- x
@@ -211,7 +190,7 @@ setMethod("dbLazyLoad", "filehash",
 
 db2env <- function(db) {
     if(is.character(db))
-        db <- dbInit(db)  ## use the default DB type
+        db <- dbInit(db)  ## use the default type
     env <- new.env(hash = TRUE)
     dbLoad(db, env)
     env
@@ -254,8 +233,6 @@ setMethod("lapply", signature(X = "filehash"),
 
 setGeneric("dbMultiFetch", function(db, key, ...) standardGeneric("dbMultiFetch"))
 setGeneric("dbReconnect", function(db, ...) standardGeneric("dbReconnect"))
-setGeneric("dbFirst", function(db, ...) standardGeneric("dbFirst"))
-setGeneric("dbNext", function(db, ...) standardGeneric("dbNext"))
 setGeneric("dbInsert", function(db, key, value, ...) standardGeneric("dbInsert"))
 setGeneric("dbFetch", function(db, key, ...) standardGeneric("dbFetch"))
 setGeneric("dbExists", function(db, key, ...) standardGeneric("dbExists"))
@@ -318,10 +295,7 @@ setMethod("[", signature(x = "filehash", i = "ANY", j = "ANY", drop = "missing")
 ## 'serialize()' changed from 2.3.0 to 2.4.0 so we need this function
 ## for back compatibility
 toBytes <- function(x) {
-    if(getRversion() < package_version("2.4.0"))
-        charToRaw(serialize(x, NULL))
-    else
-        serialize(x, NULL)
+    serialize(x, NULL)
 }
 
 
