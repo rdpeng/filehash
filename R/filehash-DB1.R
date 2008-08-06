e6d1e61375abd72b89c9a8c920793fafa4e3fa53
Roger D. Peng [amelia] <rpeng@jhsph.edu>
2008-08-06 16:25:23 -0400

    Copyrights

--- R/filehash-DB1.R
+++ R/filehash-DB1.R
@@ -1,5 +1,5 @@
 ######################################################################
-## Copyright (C) 2006, Roger D. Peng <rpeng@jhsph.edu>
+## Copyright (C) 2006--2008, Roger D. Peng <rpeng@jhsph.edu>
 ##     
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
