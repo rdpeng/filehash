0723d539fd2e18f611dde9e4f5c0c770282db24c
Roger D. Peng [amelia] <rpeng@jhsph.edu>
2008-08-06 16:21:59 -0400

    Indenting

--- R/dump.R
+++ R/dump.R
@@ -18,45 +18,45 @@
 #####################################################################
 
 dumpImage <- function(dbName = "Rworkspace", type = NULL) {
-    dumpObjects(list = ls(envir = globalenv(), all.names = TRUE),
-                dbName = dbName, envir = globalenv())
+        dumpObjects(list = ls(envir = globalenv(), all.names = TRUE),
+                    dbName = dbName, type = type, envir = globalenv())
 }
 
 dumpObjects <- function(..., list = character(0), dbName, type = NULL,
                         envir = parent.frame()) {
-    names <- as.character(substitute(list(...)))[-1]
-    list <- c(list, names)
-    if(!dbCreate(dbName, type))
-        stop("could not create database file")
-    db <- dbInit(dbName, type)
+        names <- as.character(substitute(list(...)))[-1]
+        list <- c(list, names)
+        if(!dbCreate(dbName, type))
+                stop("could not create database file")
+        db <- dbInit(dbName, type)
 
-    for(i in seq(along = list)) 
-        dbInsert(db, list[i], get(list[i], envir))
-    db
+        for(i in seq(along = list)) 
+                dbInsert(db, list[i], get(list[i], envir))
+        db
 }
 
 dumpDF <- function(data, dbName = NULL, type = NULL) {
-    if(is.null(dbName))
-        dbName <- as.character(substitute(data))
-    dumpList(as.list(data), dbName = dbName, type = type)
+        if(is.null(dbName))
+                dbName <- as.character(substitute(data))
+        dumpList(as.list(data), dbName = dbName, type = type)
 }
 
 dumpList <- function(data, dbName = NULL, type = NULL) {
-    if(!is.list(data))
-        stop("'data' must be a list")
-    vnames <- names(data)
-    
-    if(is.null(vnames) || isTRUE("" %in% vnames))
-        stop("list must have non-empty names")
-    if(is.null(dbName))
-        dbName <- as.character(substitute(data))
-    
-    if(!dbCreate(dbName, type))
-        stop("could not create database file")
-    db <- dbInit(dbName, type)
+        if(!is.list(data))
+                stop("'data' must be a list")
+        vnames <- names(data)
+        
+        if(is.null(vnames) || isTRUE("" %in% vnames))
+                stop("list must have non-empty names")
+        if(is.null(dbName))
+                dbName <- as.character(substitute(data))
+        
+        if(!dbCreate(dbName, type))
+                stop("could not create database file")
+        db <- dbInit(dbName, type)
 
-    for(i in seq(along = vnames))
-        dbInsert(db, vnames[i], data[[vnames[i]]])
-    db
+        for(i in seq(along = vnames))
+                dbInsert(db, vnames[i], data[[vnames[i]]])
+        db
 }
 
