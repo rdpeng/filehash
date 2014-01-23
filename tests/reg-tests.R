11099badf53594c8cedb18f155e19a72ba0c7f8b
Gregory Jefferis <jefferis@gmail.com>
2014-01-23 01:15:32 +0000

    regression test for rds2

--- tests/reg-tests.R
+++ tests/reg-tests.R
@@ -46,6 +46,45 @@ db <- dbInit("testRDSdb/", "RDS")
 print(db)
 
 ######################################################################
+## Test 'filehashRDS2' class
+
+dbCreate("mydbRDS2", "RDS2")
+db <- dbInit("mydbRDS2", "RDS2")
+show(db)
+
+## Put some data into it
+set.seed(1000)
+dbInsert(db, "a", 1:10)
+dbInsert(db, "b", rnorm(100))
+dbInsert(db, "c", 100:1)
+dbInsert(db, "d", runif(1000))
+dbInsert(db, "other", "hello")
+
+dbList(db)
+
+dbExists(db, "e")
+dbExists(db, "a")
+
+env <- db2env(db)
+ls(env)
+
+env$a
+env$b
+env$c
+str(env$d)
+env$other
+
+env$b <- rnorm(100)
+mean(env$b)
+
+env$a[1:5] <- 5:1
+print(env$a)
+
+dbDelete(db, "c")
+
+tryCatch(print(env$c), error = function(e) cat(as.character(e)))
+tryCatch(dbFetch(db, "c"), error = function(e) cat(as.character(e)))
+######################################################################
 ## test filehashDB1 class
 
 dbCreate("mydb", "DB1")
