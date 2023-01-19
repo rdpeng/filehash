e56d33e6622f10f4efd4e2b5e4120c44dc55a994
Roger D. Peng <rdpeng@gmail.com>
2023-01-19 14:14:47 -0600

    Remove C-based implementation of SHA1 hash

--- R/hash.R
+++ R/hash.R
@@ -1,10 +1,9 @@
 sha1 <- function(object, skip = 14L) {
-	## Setting 'skip = 14' gives us the same results as
-	## 'digest(object, "sha1")'
-	bytes <- serialize(object, NULL)
-	.Call(C_sha1_object, bytes, skip)
+        bytes <- serialize(object, NULL)
+        digest(bytes, algo = "sha1", skip = skip, serialize = FALSE)
 }
 
+
 sha1_file <- function(filename, skip = 0L) {
-	.Call(C_sha1_file, filename, skip)
+	digest(filename, algo = "sha1", file = TRUE)
 }
