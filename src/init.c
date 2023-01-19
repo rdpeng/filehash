d6c8a986a5bb89c0101a89076c1fc4b17cf6e060
Roger D. Peng <rdpeng@gmail.com>
2023-01-19 14:19:16 -0600

    Remove sha1 functions from C function table

--- src/init.c
+++ src/init.c
@@ -2,16 +2,12 @@
 #include <R_ext/Rdynload.h>
 #include <Rinternals.h>
 
-SEXP sha1_object(SEXP object, SEXP skip_bytes);
-SEXP sha1_file(SEXP filename, SEXP skip_bytes);
 SEXP read_key_map(SEXP filename, SEXP map, SEXP filesize, SEXP pos);
 SEXP lock_file(SEXP filename);
 
 static const R_CallMethodDef CallEntries[] = {
 	{"lock_file", (DL_FUNC) &lock_file, 1},
 	{"read_key_map", (DL_FUNC) &read_key_map, 4},
-	{"sha1_object", (DL_FUNC) &sha1_object, 2},
-	{"sha1_file", (DL_FUNC) &sha1_file, 2},
 	{NULL, NULL, 0}
 };
 
