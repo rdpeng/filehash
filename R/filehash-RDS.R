862f8eec5432628ee37c1edf109ffd67e3cad5b9
Roger D. Peng <rdpeng@gmail.com>
2023-01-21 13:30:22 -0500

    Update copyright

--- R/filehash-RDS.R
+++ R/filehash-RDS.R
@@ -1,5 +1,5 @@
-######################################################################
-## Copyright (C) 2006, Roger D. Peng <rpeng@jhsph.edu>
+##########################################################################
+## Copyright (C) 2006-2023, Roger D. Peng <roger.peng @ austin.utexas.edu>
 ##     
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
 ## along with this program; if not, write to the Free Software
 ## Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 ## 02110-1301, USA
-#####################################################################
+##########################################################################
 
 ################################################################################
 ## Class 'filehashRDS'
